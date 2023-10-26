970e5d17cf2f2cfcf95cfc2a7b7c4b77dd9ee283
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-10-26 20:39:45 +0200

    Get it working on Linux and add .gitignore

--- naive_cpp/compute.hpp
+++ naive_cpp/compute.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <complex>
+#include <algorithm>
+
+
 struct Resolution {
 	int Width, Height;
 };
@@ -9,4 +13,32 @@ struct View {
 	double zoom;
 };
 
-int* compute(Resolution resolution, View view, int maxIterations);
+int *compute(Resolution resolution, View view, int maxIterations) {
+	int size = std::max(resolution.Width, resolution.Height);
+	double scale = 3 / view.zoom;
+
+	int* iterations = new int[resolution.Width * resolution.Height];
+
+	int index = 0;
+	for (int y = 0; y < resolution.Height; y++) {
+		for (int x = 0; x < resolution.Width; x++) {
+			std::complex<double> c = std::complex<double>(
+				(x + .5 - resolution.Width / 2) / size * scale + view.center_x,
+				(y + .5 - resolution.Height / 2) / size * scale + view.center_y
+			);
+			std::complex<double> z;
+
+			int iteration;
+			for (iteration = 0; iteration < maxIterations; iteration++) {
+				z = z * z + c;
+				if (z.real() * z.real() + z.imag() * z.imag() > 2) {
+					break;
+				}
+			}
+
+			iterations[index++] = iteration;
+		}
+	}
+
+	return iterations;
+}
--- naive_cpp/render.hpp
+++ naive_cpp/render.hpp
@@ -1,6 +1,5 @@
 #pragma once
-#include <SDL.h>
-#undef main
+#include <SDL2/SDL.h>
 
 #include "compute.hpp"
 #include <cstdint>
